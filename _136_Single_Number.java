d8ea9ef1d58cfd9891d2a43c454ae80458b50168
Vi Le <lecongvi18052002@gmail.com>
2023-08-30 07:39:57 +0700

    second commit

--- _136_Single_Number.java
+++ _136_Single_Number.java
@@ -1,28 +1,22 @@
-import java.util.Arrays;
 public class _136_Single_Number {
-    public static int singleNumber(int[] a) {
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 1; i += 3) {
-            // Limit is length-2 because if all case is failure
-            // Then value of last element is only one
-            // Because don't have next element equal with last element
-            if (nums[i] == nums[i + 1]) {
+    public int singleNumber(int[] a) {
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (a[i] == a[j]) {
+                    count++;
+                }
+            }
+
+            if (count > 1) {
+                count = 0;
                 continue;
             } else {
-                return nums[i];
+                return a[i];
             }
         }
-        return nums[nums.length - 1];
-    }
-
-    public static void main(String[] args) {
-        int[] nums = { 2, 2, 1 };
-        int[] arr = { 4, 1, 2, 1, 2 };
-
-        int result = singleNumber(nums);
-        System.out.println("Test Case 1: " + result);
-
-        result = singleNumber(arr);
-        System.out.println("Test Case 2: " + result);
+        System.gc();
+        return -1;
     }
 }
